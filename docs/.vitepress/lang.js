cf18a1a46a3d402820187059aeca3718b2ebbcb5
陈关印 <chenguanyin@digitforce.com>
2022-01-06 18:29:33 +0800

    ✨ feat: 分页，表格的样式修改，按钮的mmin-width修改

--- docs/.vitepress/lang.js
+++ docs/.vitepress/lang.js
@@ -1,14 +1,14 @@
 /*
  * @Author: saber
  * @Date: 2022-01-04 14:25:35
- * @LastEditTime: 2022-01-04 16:04:40
- * @LastEditors: saber
+ * @LastEditTime: 2022-01-06 10:26:44
+ * @LastEditors: chenguanyin
  * @Description: 
  */
 ;(() => {
   const supportedLangs = window.supportedLangs
   const cacheKey = 'preferred_lang'
-  const defaultLang = 'zh-CN'
+  const defaultLang = 'en-US'
   // docs supported languages
   const langAlias = {
     en: 'en-US',
